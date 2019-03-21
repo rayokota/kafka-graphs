ac0b77a407a66ed352975437d6770f526bc8969d
Robert Yokota <rayokota@gmail.com>
2019-03-21 13:45:08 -0700

    Minor optimization

--- kafka-graphs-core/src/main/java/io/kgraph/pregel/PregelComputation.java
+++ kafka-graphs-core/src/main/java/io/kgraph/pregel/PregelComputation.java
@@ -498,13 +498,7 @@ public class PregelComputation<K, VV, EV, Message> implements Closeable {
                             if (ZKUtils.isReady(curator, applicationId, pregelState)) {
                                 Map<K, Map<K, List<Message>>> messages = localworkSetStore.get(pregelState.superstep());
                                 if (hasVerticesToForward(messages)) {
-                                    // This check is to ensure we have all messages produced in the last stage;
-                                    // we may get new messages as well but that is fine
-                                    Map<Integer, Long> lastWrittenOffsets =
-                                        (Map<Integer, Long>) previousAggregates(pregelState.superstep()).get(LAST_WRITTEN_OFFSETS);
-                                    if (isTopicSynced(internalConsumer, workSetTopic, pregelState.superstep(), lastWrittenOffsets)) {
-                                        forwardVertices(messages);
-                                    }
+                                    forwardVertices(messages);
                                 }
 
                                 // clean up previous step
